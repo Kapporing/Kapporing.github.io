02532ce15a249624bffa0ee30be815b0870d6968
KapporingPC <ronaldwilliamwidjaja@yahoo.com>
2020-06-10 15:46:37 -0700

    Updates

---
+++ precache-manifest.3e2f1e6dd6b08f696d83d86bde0aa43d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "677114aa5d308cdbbe70a7817efbe827",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4206650dde4fdd12ce93",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4f56abd9a25999e89607",
+    "url": "/static/css/main.93ea63ae.chunk.css"
+  },
+  {
+    "revision": "4206650dde4fdd12ce93",
+    "url": "/static/js/2.32495053.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.32495053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f56abd9a25999e89607",
+    "url": "/static/js/main.5817aeca.chunk.js"
+  },
+  {
+    "revision": "54f30fcaea9500ce404a",
+    "url": "/static/js/runtime-main.b4c216f6.js"
+  }
+]);
